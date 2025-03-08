e4ed1ab5da46f39870d6a749fc4c02f0c1228370
Kevin Baer <149030800+kevbaer@users.noreply.github.com>
2025-03-08 15:15:10 -0800

    Add My Funcs

---
+++ R/funcs.R
@@ -0,0 +1,32 @@
+#' RLIFF!
+#'
+#' @returns RLIFF
+#' @export
+#'
+#' @examples
+#' RLIFF()
+RLIFF <- function() {
+  cat("Remember Love Inspire Friends Family")
+}
+
+#' HAMILTON!
+#'
+#' @returns Hamilton
+#' @export
+#'
+#' @examples
+#' HAMILTON()
+HAMILTON <- function() {
+  cat("Dying is Easy, Young Ma?n, Living is Harder.")
+}
+
+#' CATCHPHRASE!
+#'
+#' @returns Catchphrase
+#' @export
+#'
+#' @examples
+#' CATCHPHRASE()
+CATCHPHRASE <- function() {
+  cat("Just Keep Dreaming <3")
+}
